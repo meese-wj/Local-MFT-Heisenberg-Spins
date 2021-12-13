afd31b6a248adf2140e70e7e76f2d7ae01607ff5
W. Joe Meese <meese022@umn.edu>
2021-12-13 10:39:14 -0600

    Same as before?

--- src/LatticeSetup.jl
+++ src/LatticeSetup.jl
@@ -27,17 +27,8 @@ total_sites( latt_params::LatticeParameters ) = latt_params.Lx * latt_params.Ly
 total_boundary_sites( latt_params::LatticeParameters ) = 2 * num_boundary_x_per_side * latt_params.Ly
 total_bulk_sites( latt_params::LatticeParameters ) = total_sites(latt_params) - total_boundary_sites(latt_params)
 site_index( site::Site2D, latt_params::LatticeParameters ) = (site.yind - 1) * latt_params.Lx + site.xind
-function site_xindex( index::Int, latt_params::LatticeParameters )
-    xind = mod(index, latt_params.Lx)
-    endtrue = xind == 0
-    xind = xind * convert(Int, !endtrue) + latt_params.Lx * convert(Int, endtrue)
-    return xind
-end
-function site_coords( index::Int, latt_params::LatticeParameters )
-    xind = site_xindex( index, latt_params ) 
-    return Site2D( xind, 1 + div( index - xind, latt_params.Lx ) )
-end
-
+site_xindex( index::Int, latt_params::LatticeParameters ) = mod(index - 1, latt_params.Lx) + 1
+site_coords( index::Int, latt_params::LatticeParameters ) = Site2D( site_xindex(index, latt_params), 1 + div( index - 1, latt_params.Lx ) )
 """
 Construct the nearest neighbor table 
 """
